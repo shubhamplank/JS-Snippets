eef67a3042e0d0018858f60952857e7afce7b9f0
shubhamplank <shubham.plank@gmail.com>
2022-04-28 07:16:02 -0700

    restructure

--- DataStructure/tree.js
+++ DataStructure/tree.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor(value){
+  constructor(value) {
     this.left = null;
     this.right = null;
     this.value = value;
@@ -7,61 +7,61 @@ class Node {
 }
 
 class BinarySearchTree {
-  constructor(){
+  constructor() {
     this.root = null;
   }
-  insert(value){
+  insert(value) {
     const newNode = new Node(value);
     if (this.root === null) {
       this.root = newNode;
     } else {
       let currentNode = this.root;
-      while(true){
-        if(value < currentNode.value){
+      while (true) {
+        if (value < currentNode.value) {
           //Left
-          if(!currentNode.left){
+          if (!currentNode.left) {
             currentNode.left = newNode;
             return this;
           }
           currentNode = currentNode.left;
         } else {
           //Right
-          if(!currentNode.right){
+          if (!currentNode.right) {
             currentNode.right = newNode;
             return this;
-          } 
+          }
           currentNode = currentNode.right;
         }
       }
     }
   }
-  lookup(value){
+  lookup(value) {
     if (!this.root) {
       return false;
     }
     let currentNode = this.root;
-    while(currentNode){
-      if(value < currentNode.value){
+    while (currentNode) {
+      if (value < currentNode.value) {
         currentNode = currentNode.left;
-      } else if(value > currentNode.value){
+      } else if (value > currentNode.value) {
         currentNode = currentNode.right;
       } else if (currentNode.value === value) {
         return currentNode;
       }
     }
-    return null
+    return null;
   }
   // remove
 }
 
 const tree = new BinarySearchTree();
-tree.insert(9)
-tree.insert(4)
-tree.insert(6)
-tree.insert(20)
-tree.insert(170)
-tree.insert(15)
-tree.insert(1)
+tree.insert(9);
+tree.insert(4);
+tree.insert(6);
+tree.insert(20);
+tree.insert(170);
+tree.insert(15);
+tree.insert(1);
 // JSON.stringify(traverse(tree.root))
 tree.lookup(15);
 tree.lookup(7);
@@ -76,6 +76,3 @@ function traverse(node) {
   tree.right = node.right === null ? null : traverse(node.right);
   return tree;
 }
-
-
-
