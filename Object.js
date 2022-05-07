22f9f96b6bd79543e855981956293b511c2b3782
shubhamplank <shbuham.plank@gmail.com>
2022-05-07 12:31:40 +0530

    merge changes

--- Object.js
+++ Object.js
@@ -6,7 +6,6 @@ const omit = (obj, keys) => Object.keys(obj).filter(key => !(keys.includes(key))
 
 const omitBy = (obj, fn) => Object.keys(obj).reduce((acc, key) => (!fn(obj[key]) ? acc[key] = obj[key] : null, acc), {})
 
-<<<<<<< HEAD
 const hasKey = (obj, keys) => keys.reduce((acc, key) => (acc = key in acc ? acc[key] : {}), obj)
 let obj = {
     a: 1,
@@ -16,7 +15,6 @@ let obj = {
 
 let demo = hasKey(obj, ['b', 'c', 'y']);
 console.log(demo)
-=======
 
 const deepGet = (obj, keys) => keys.reduce((acc, key) => (acc = acc && acc[key] !== null && acc[key] !== undefined ? acc[key] : null, acc), obj)
 
@@ -37,4 +35,3 @@ deepGet(data, ['foo', 'bar', 'baz', 8]); // null
 let demo = deepGet(data, ['foo', 'bar', 'baz', 6]); // get 3
 console.log(demo)
 
->>>>>>> 5704e583e6a2744e1ab7ef6a0361258012a33f09
