e9cb3a8b4e91baaad9ad29e5bdd643fa8905edfb
Shubham Agarwal <shubham.plank@gmail.com>
2024-05-24 19:05:37 +0530

    Update and rename useRequest.js to Hooks.js

---
+++ Mislaneous/Hooks.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+
+export function useToggle(on) { 
+  const [toggle, setToggle ] = useState(on);
+  const handleToggle = ()=> setToggle(prevState => !prevState);
+
+  return [toggle, handleToggle];
+}
+
+//Debounce a value 
+
+export function useDebounce(value, delay) {
+  const [debounced, setDebounced] = useState(value)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebounced(value)
+    }, delay);
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [value, delay])
+
+  return debounced
+
+}
+
+
+// Debounce a function
+export function useDebounce(func, delay) {
+  const debouncedFunction = React.useRef(null);
+
+  return useCallback(
+    (...args) => {
+      clearTimeout(debouncedFunction.current);
+      debouncedFunction.current = setTimeout(() => {
+        func(...args);
+      }, delay);
+    },
+    [func, delay]
+  );
+}
+
+export function App() {
+  const [value, setValue] = useState(0);
+
+  const increment = useDebounce(() => setValue(state => state + 1), 2000);
+
+  return (
+    <>
+      <div>
+        <button onClick={increment}>inc</button>
+        <h2>value: {value}</h2>
+      </div>
+    </>
+  );
+}
