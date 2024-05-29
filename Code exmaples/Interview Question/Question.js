50d29d62c7f3c61b07d0d991578bae03a2e90600
shubhamplank <shubham.plank@gmail.com>
2024-05-29 15:22:43 +0530

    interview question

---
+++ Code exmaples/Interview Question/Question.js
@@ -0,0 +1,24 @@
+//-------------------------------Mpokket : Round 1 -------------------
+
+// Question 1 :implement this:
+obj.initializeVal(10).add(5).subtract(3).result()
+obj.initializeVal(10).subtract(5).add(3).result()
+
+// Question 2:
+
+/*Given an integer array nums and an integer k, return the kth largest element in the array.
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+Can you solve it without sorting? 
+
+Example 1:
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+*/
+
+// --------------------------------Mercari Round 1:--------------------
+// #1. Create a Tab UI in react. (15 min + 10min)
+// #2. Given a list array, render it and a input box to filter the list, in vanilla js. (15 min + 10min)
